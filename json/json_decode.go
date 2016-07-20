9929258cd81210d845d7ecd7abb0fcecc81b72e5
Sergei <sleepytomcat@gmail.com>
2016-07-19 22:40:23 -0700

    json decoding example

---
+++ json/json_decode.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+    "encoding/json"
+    "fmt"
+)
+
+func main() {
+    var animalsJson = []byte(`[{"Name": "Platypus", "Order": "Monotremata"}, {"Name": "Quoll",    "Order": "Dasyuromorphia"}]`)
+
+
+    var decoded interface{}
+    err := json.Unmarshal(animalsJson, &decoded)
+    if err != nil {
+	fmt.Println("error:", err)
+    }
+
+    fmt.Println(decoded)
+}
